d6b54d9f3506c935311f40883b2e516703e81a3c
Kaique Parente <81454096+Kaique-Parente@users.noreply.github.com>
2023-12-18 23:12:12 -0300

    Add files via upload

--- course/src/aplicattion/Program.java
+++ course/src/aplicattion/Program.java
@@ -21,6 +21,25 @@ public class Program {
         System.out.print("Quantity in stock: ");
         product.quantity = e.nextInt();
         
-        System.out.println(product);
+        System.out.println("");
+        System.out.println("Product Data: " + product);
+        
+        System.out.println("");
+        System.out.print("Enter the number of products to be added in stock: ");
+        int quantity = e.nextInt();
+        
+        product.addProducts(quantity);
+        
+        System.out.println("");
+        System.out.println("Updated Data: " + product);
+        
+        System.out.println("");
+        System.out.print("Enter the number of products to be removed from stock: ");
+        quantity = e.nextInt();
+        
+        product.removeProducts(quantity);
+        
+        System.out.println("");
+        System.out.println("Updated Data: " + product);
     }
 }
--- course/src/entities/Product.java
+++ course/src/entities/Product.java
@@ -20,7 +20,7 @@ public class Product {
     }
     
     public String toString() {
-        return name + ", $ " + 
+        return  name + ", $ " + 
                 String.format("%.2f", price) + 
                 ", " + 
                 quantity + " units, Total: " + 
