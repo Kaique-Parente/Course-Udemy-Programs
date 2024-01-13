083d2532a1407178e72592584b147149f52ff6b7
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-12 22:40:12 -0300

    Bloco try-with-resources

--- Sc17_Archives/src/application/Program.java
+++ Sc17_Archives/src/application/Program.java
@@ -7,34 +7,18 @@ import java.io.IOException;
 public class Program {
 
     public static void main(String[] args) {
-
+        
         String path = "C:\\Archives\\in.txt";
-        FileReader fr = null;
-        BufferedReader br = null;
 
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr); // instanciado a partir do fr, + flexível
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))){
             String line = br.readLine(); // Se o arquivo já estiver no final, vai me retornar null.
-                                     
+
             while (line != null) {
                 System.out.println(line);
                 line = br.readLine();
             }
         } catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        } finally { // Fechar as duas streams
-            try {
-                if (fr != null) {
-                    fr.close();
-                }
-                if (br != null) {
-                    br.close();
-                }
-            } catch (IOException e) { // Abrindo e fechando manualmente
-                e.getStackTrace();
-            }
         }
     }
 }
