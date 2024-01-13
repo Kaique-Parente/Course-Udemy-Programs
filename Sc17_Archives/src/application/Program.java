470f5010e15e875e8c3e5e163ea688b5924251d7
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-12 22:09:20 -0300

    File and Scanner

--- Sc17_Archives/src/application/Program.java
+++ Sc17_Archives/src/application/Program.java
@@ -1,7 +1,26 @@
 package application;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Program {
+
     public static void main(String[] args) {
         
+        File file = new File("C:\\Archives\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {  // Enquanto existir linha no arquivo
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally { // independente do try ou do catch o meu scanner vai fechar]
+            if(sc != null) {
+                sc.close();
+            }
+        }
     }
 }
