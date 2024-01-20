88d21a060b6a045c8c3a73d246dcbb0809f7f087
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-19 21:25:32 -0300

    Created initial model.entities

--- Sc18_Interfaces/src/application/Program.java
+++ Sc18_Interfaces/src/application/Program.java
@@ -1,7 +1,28 @@
 package application;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+import model.entities.CarRental;
+import model.entities.Vehicle;
+
 public class Program {
     public static void main(String[] args) {
         
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        DateTimeFormatter fm1 = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        
+        System.out.println("Enter rental data");
+        System.out.print("Car model: ");
+        String model = sc.nextLine();
+        
+        System.out.print("Pickup (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.next(), fm1);
+        System.out.print("Return (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.next(), fm1);
+        CarRental cr = new CarRental(start, finish, new Vehicle(model));
     }
 }
