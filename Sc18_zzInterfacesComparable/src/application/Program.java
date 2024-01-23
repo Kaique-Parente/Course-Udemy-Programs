d9cfa1175c52ee3a01928c5262c2529b42a788b3
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-23 14:34:19 -0300

    CompareTo Employee

--- Sc18_zzInterfacesComparable/src/application/Program.java
+++ Sc18_zzInterfacesComparable/src/application/Program.java
@@ -6,24 +6,29 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
+import model.entities.Employee;
 
 public class Program {
     public static void main(String[] args) {
         
         String path = "C:\\Archives\\in.txt";
-        List<String> list = new ArrayList<>();
+        List<Employee> list = new ArrayList<>();
+        Locale.setDefault(Locale.US);
         
         try(BufferedReader br = new BufferedReader(new FileReader(path))) {
-           String name = br.readLine();
            
-           while(name != null) {
-               list.add(name);
-               name = br.readLine();
+           String employeeCsv = br.readLine();
+           
+           while(employeeCsv != null) {
+               String[] fields = employeeCsv.split(",");
+               list.add(new Employee(fields[0], Double.parseDouble(fields[1])));
+               employeeCsv = br.readLine();
            }
            Collections.sort(list);
            
-           for(String x: list) {
-               System.out.println(x);
+           for(Employee x: list) {
+               System.out.println(x.getName() + ", " + String.format("%.2f", x.getSalary()));
            }
         }
         catch(IOException e) {
