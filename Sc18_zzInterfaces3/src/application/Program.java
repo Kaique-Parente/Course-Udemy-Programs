c9a1b883b61703152f7a6620656e040f7b63418c
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-23 12:31:21 -0300

    Initial Commit

---
+++ Sc18_zzInterfaces3/src/application/Program.java
@@ -0,0 +1,19 @@
+package application;
+
+import devices.Printer;
+import devices.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Printer p = new Printer("1080");
+		p.processDoc("My Letter");
+		p.print("My Letter");
+                System.out.println("");
+                
+		Scanner s = new Scanner("2003");
+		s.processDoc("My Email");
+		System.out.println("Scan result: " + s.scan());
+	}
+}
