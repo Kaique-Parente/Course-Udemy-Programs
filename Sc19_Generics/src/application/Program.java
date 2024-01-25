755707d8579cf479bf41241b4ff02d88aad842d0
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-24 22:10:42 -0300

    PrintServiceString

--- Sc19_Generics/src/application/Program.java
+++ Sc19_Generics/src/application/Program.java
@@ -1,19 +1,20 @@
 package application;
 
 import java.util.Scanner;
-import services.PrintService;
+import services.PrintServiceString;
 
 public class Program {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        PrintService ps = new PrintService();
+        PrintServiceString ps = new PrintServiceString();
         
         System.out.print("How many values? ");
         int v = sc.nextInt();
+        sc.nextLine();
         
         for(int i=1; i <= v; i++) {
-            ps.addValue(sc.nextInt());
+            ps.addValue(sc.nextLine());
         }
         ps.print();
         System.out.println("First: " + ps.first());
---
+++ Sc19_Generics/src/services/PrintServiceString.java
@@ -0,0 +1,32 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrintServiceString {
+
+    List<String> list = new ArrayList<>();
+
+    public void addValue(String value) {
+        list.add(value);
+    }
+
+    public String first() {
+        if (list.isEmpty()) {
+            throw new IllegalStateException("List is empty");
+        }
+        return list.get(0);
+    }
+
+    public void print() {
+        System.out.print("[");
+        if (!list.isEmpty()) {
+            System.out.print(list.get(0));
+        }
+        
+        for (int i=1; i < list.size(); i++) {
+            System.out.print(", " + list.get(i));
+        }
+        System.out.println("]");
+    }
+}
