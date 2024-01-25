4e05a7060a90e358f7cdc2a8604142f1e8608026
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-24 21:01:13 -0300

    Default Methods

--- Sc18_zzzDefaultMethods/src/application/Program.java
+++ Sc18_zzzDefaultMethods/src/application/Program.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 import java.util.Scanner;
 
 import services.BrazilInterestService;
+import services.InterestService;
 import services.UsaInterestService;
 
 public class Program {
@@ -18,7 +19,8 @@ public class Program {
 		System.out.print("Months: ");
 		int months = sc.nextInt();
 
-		UsaInterestService is = new UsaInterestService(1.0);
+		InterestService is = new UsaInterestService(1.0);
+                InterestService av = new BrazilInterestService(2.0);
 		double payment = is.payment(amount, months);
 
 		System.out.println("Payment after " + months + " months:");
--- Sc18_zzzDefaultMethods/src/services/UsaInterestService.java
+++ Sc18_zzzDefaultMethods/src/services/UsaInterestService.java
@@ -1,8 +1,6 @@
 package services;
 
-import java.security.InvalidParameterException;
-
-public class UsaInterestService {
+public class UsaInterestService implements InterestService{
 
 	private double interestRate;
 
@@ -10,14 +8,8 @@ public class UsaInterestService {
 		this.interestRate = interestRate;
 	}
 
+        @Override
 	public double getInterestRate() {
 		return interestRate;
-	}
-
-	public double payment(double amount, int months) {
-		if (months < 1) {
-			throw new InvalidParameterException("Months must be greater than zero");
-		}
-		return amount * Math.pow(1.0 + interestRate / 100.0, months);
 	}
 }
