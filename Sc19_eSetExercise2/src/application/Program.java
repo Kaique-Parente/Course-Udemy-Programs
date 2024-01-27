3e34da4a13c9ccabd7ce4ef87d9ea529a75dff36
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-26 23:19:09 -0300

    My solution

--- Sc19_eSetExercise2/src/application/Program.java
+++ Sc19_eSetExercise2/src/application/Program.java
@@ -1,13 +1,37 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package application;
 
-/**
- *
- * @author kaiqu
- */
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import model.entities.Student;
+
 public class Program {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        Set<Student> set = new HashSet<>();
+        
+        System.out.print("How many students for course A? ");
+        int a = sc.nextInt();
+        
+        for(int i = 1; i <= a; i++) {
+            set.add(new Student(sc.nextInt()));
+        }
+        
+        System.out.print("How many students for course B? ");
+        int b = sc.nextInt();
+        
+        for(int i = 1; i <= b; i++) {
+            set.add(new Student(sc.nextInt()));
+        }
+        
+        System.out.print("How many students for course C? ");
+        int c = sc.nextInt();
+        
+        for(int i = 1; i <= c; i++) {
+            set.add(new Student(sc.nextInt()));
+        }
+        
+        System.out.println("Total students: " + set.size());
+    }
 }
