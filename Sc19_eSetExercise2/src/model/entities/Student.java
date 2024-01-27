d460f1bc79a495adad656d918aa46049d65a2fff
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-26 23:09:20 -0300

    Student

---
+++ Sc19_eSetExercise2/src/model/entities/Student.java
@@ -0,0 +1,42 @@
+
+package model.entities;
+
+import java.util.Objects;
+
+public class Student {
+    private Integer id;
+
+    public Student(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 31 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Student other = (Student) obj;
+        return Objects.equals(this.id, other.id);
+    }    
+}
