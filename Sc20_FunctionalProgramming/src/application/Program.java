db9ba0f4a1ddfdde3b16593d920045077731fea0
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-29 20:22:45 -0300

    Method reference non static

--- Sc20_FunctionalProgramming/src/application/Program.java
+++ Sc20_FunctionalProgramming/src/application/Program.java
@@ -17,7 +17,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
 
-        list.removeIf(Product::staticProductPredicate);
+        list.removeIf(Product::nonStaticProductPredicate);
 
         for (Product x : list) {
             System.out.println(x);
--- Sc20_FunctionalProgramming/src/entities/Product.java
+++ Sc20_FunctionalProgramming/src/entities/Product.java
@@ -31,6 +31,10 @@ public class Product {
         return p.getPrice() >= 100.0;
     }
     
+    public boolean nonStaticProductPredicate() {
+        return price >= 100.0;
+    }
+    
     @Override
     public String toString() {
         return "Name= " + name + ", Price= " + price;
