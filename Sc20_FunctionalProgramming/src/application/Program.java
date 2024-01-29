81a5b6c2c4ab252e7869a6b9da8564f92ac167da
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-29 20:30:43 -0300

    Declared lambda expression

--- Sc20_FunctionalProgramming/src/application/Program.java
+++ Sc20_FunctionalProgramming/src/application/Program.java
@@ -4,6 +4,7 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
 public class Program {
 
@@ -16,8 +17,12 @@ public class Program {
         list.add(new Product("Mouse", 50.00));
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
+        
+        double min = 100.0;
+        
+        Predicate<Product> pred = p -> p.getPrice() >= min;
 
-        list.removeIf(Product::nonStaticProductPredicate);
+        list.removeIf(pred);
 
         for (Product x : list) {
             System.out.println(x);
