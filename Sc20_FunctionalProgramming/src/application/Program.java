342ce39b12dadc9508690f24068129b085fcde75
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-29 18:28:22 -0300

    Simplified Solution

--- Sc20_FunctionalProgramming/src/application/Program.java
+++ Sc20_FunctionalProgramming/src/application/Program.java
@@ -12,7 +12,7 @@ public class Program {
         list.add(new Product("Tv", 900.00));
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
-
+      
         list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
         
         for(Product x: list) {
