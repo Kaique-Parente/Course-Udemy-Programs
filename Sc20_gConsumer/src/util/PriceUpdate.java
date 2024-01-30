3d79b2389c1d3f6d14a6bccd362a88f37671b260
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-29 21:17:21 -0300

    Predicate implementation

---
+++ Sc20_gConsumer/src/util/PriceUpdate.java
@@ -0,0 +1,13 @@
+package util;
+
+import entities.Product;
+import java.util.function.Consumer;
+
+public class PriceUpdate implements Consumer<Product>{
+
+    @Override
+    public void accept(Product p) {
+        p.setPrice(p.getPrice() * 1.1);
+    }
+    
+}
