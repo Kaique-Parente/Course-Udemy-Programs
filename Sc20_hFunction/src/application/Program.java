52fbb58be958dea336475b870975f4f34dd022fd
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-30 16:25:17 -0300

    Interface implementation

---
+++ Sc20_hFunction/src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import entities.Product;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+import util.UpperCaseName;
+
+public class Program {
+
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+        
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+        
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+        
+        names.forEach(System.out::println);
+    }
+}
