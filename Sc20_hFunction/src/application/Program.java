7fbebf9eaf2eb5fc250164e47859f0b371787b72
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-30 16:27:01 -0300

    Method reference static

--- Sc20_hFunction/src/application/Program.java
+++ Sc20_hFunction/src/application/Program.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
-import util.UpperCaseName;
 
 public class Program {
 
@@ -19,7 +18,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
         
-        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+        List<String> names = list.stream().map(Product::staticUpperCaseName).collect(Collectors.toList());
         
         names.forEach(System.out::println);
     }
