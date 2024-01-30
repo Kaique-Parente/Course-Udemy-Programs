20f92361beccce3d850fb87af8e141a807ac42f1
Kaique Parente <kaique.parente.game@outlook.com>
2024-01-30 16:30:57 -0300

    Inline lambda expression

--- Sc20_hFunction/src/application/Program.java
+++ Sc20_hFunction/src/application/Program.java
@@ -4,7 +4,6 @@ import entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Program {
@@ -19,9 +18,7 @@ public class Program {
         list.add(new Product("Tablet", 350.50));
         list.add(new Product("HD Case", 80.90));
         
-        Function<Product,String> func = p -> p.getName().toUpperCase();
-        
-        List<String> names = list.stream().map(func).collect(Collectors.toList());
+        List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
         
         names.forEach(System.out::println);
     }
