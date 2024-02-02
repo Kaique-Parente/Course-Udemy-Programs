f89642f7a545ecf975d155bf80f79509dd7c962e
Kaique Parente <kaique.parente.game@outlook.com>
2024-02-01 21:22:54 -0300

    Insert 2 names

---
+++ Sc21_jdbc3/src/application/Program.java
@@ -0,0 +1,69 @@
+package application;
+
+
+import db.DB;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class Program {
+    public static void main(String[] args) {
+        
+        Connection conn = null;
+        PreparedStatement st = null;
+        
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        
+        try{
+            conn = DB.getConnection();
+            /*
+            
+            st = conn.prepareStatement(
+                    "INSERT INTO seller"
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                    + "VALUES"
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            
+            st.setString(1, "Carl");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("22/04/1985").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+            
+            int rowsAffected = st.executeUpdate();
+            */
+            
+            st = conn.prepareStatement(
+                    "INSERT INTO department"
+                    + "(Name)"
+                    + "VALUES"
+                    + "('D1'), ('D2')",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            
+            int rowsAffected = st.executeUpdate();
+            
+            if(rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            } else {
+                System.out.println("No rowsAffected!");
+            }
+          
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
+       
+        
+    }
+}
