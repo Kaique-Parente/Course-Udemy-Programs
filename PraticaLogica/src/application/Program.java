e31ca244cd949feb08d73da43e79bd30a685351f
Kaique Parente <kaique.parente.game@outlook.com>
2024-03-29 19:46:38 -0300

    Verificar se na frase existe todas as letras do alfabeto

--- PraticaLogica/src/application/Program.java
+++ PraticaLogica/src/application/Program.java
@@ -1,9 +1,32 @@
 package application;
 
-
 public class Program {
-    public static long flippingBits(long n) {
-        return (long)Math.pow(2,32) + (~n);
 
+    public static void main(String[] args) {
+        String s = "We promptly judged antique ivory buckles for the prize";
+        System.out.println(pangrams(s));
+    }
+    
+    public static String pangrams(String s) {
+
+        String msg = "pangram";
+        s = s.replace(" ", "");
+
+        int letras[] = new int[26];
+        int pos = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            pos = (int) (s.toUpperCase().charAt(i) - 65);
+
+            letras[pos]++;
+        }
+
+        for (int i = 0; i < letras.length; i++) {
+            if (letras[i] == 0) {
+                msg = "not pangram";
+            }
+        }
+
+        return msg;
     }
 }
